6f56db77a5c259936872892120b9d6a302de4568
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2024-08-23 09:39:32 +0900

    Title: 황금비율 토스트, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240823/황금비율 토스트/golden-toast.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        String bread = sc.next();
+        LinkedList<Character> l = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            l.add(bread.charAt(i));
+        }
+
+        ListIterator<Character> it = l.listIterator(l.size());
+        for (int i = 0; i < m; i++) {
+            String command = sc.next();
+            if (command.equals("L")) {
+                it.previous();
+            }else if (command.equals("P")) {
+                it.add(sc.next().charAt(0));
+            }else if (command.equals("R")) {
+                it.next();
+            }else if (command.equals("D")) {
+                it.next();
+                it.remove();
+            }
+        }
+        for (char c : l) {
+            System.out.print(c);
+        }
+    }
+}
