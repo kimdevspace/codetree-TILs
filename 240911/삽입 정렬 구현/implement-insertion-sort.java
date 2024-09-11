c46e514555bf65044e7b892e6af05d6bc0a5d73c
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2024-09-11 16:48:39 +0900

    Title: 삽입 정렬 구현, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240911/삽입 정렬 구현/implement-insertion-sort.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        //입력
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        //삽입 정렬
+        insertSort(arr);
+        print(arr);
+
+    }
+
+    private static void insertSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int j = i - 1;
+            int key = arr[i];
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    private static void print(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
